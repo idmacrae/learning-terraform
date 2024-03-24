e54ba26b3c5a006938b2cc63f1d899107c5f7d8a
Ian MacRae <ian.macrae@educationhorizons.com>
2024-03-24 17:21:22 +1100

    Fix errors

--- main.tf
+++ main.tf
@@ -29,38 +29,34 @@ module "blog_vpc" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  name = "blog-alb"
 
-  target_groups = [{
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
     {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = {
-        target_id = aws_instance.blog.id
-        port = 80
-      }
     }
-  }]
+  ]
 
-  listeners = {
-    http_tcp_listeners = [
-      {
-        port     = 80
-        protocol = "HTTP"
-        target_group_index = 0
-      }
-    ]
-
-  }
-
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
