18835b1c347235ad578893f44fdbe646b84de715
Ian MacRae <ian.macrae@educationhorizons.com>
2024-03-24 18:10:24 +1100

    Update version of alb

--- module/blog/main.tf
+++ module/blog/main.tf
@@ -48,7 +48,7 @@ module "blog_autoscaling" {
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  version = "9.8.0"
 
   name = "${var.environment.name}-blog-alb"
 
